2bd47a1a50d7bdc636e90c1f820cfffb27472023
Drew Bradford <16455249+DrewBradfordXYZ@users.noreply.github.com>
2025-01-31 14:08:30 -0500

    navigation scrolling underline

---
+++ js/main.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // When a user scrolls through the projects,
+  // the corresponding navigation link will be
+  // visually highlighted, providing a clear indicator of
+  // the current section to the user.
+  function linkHighlighting(targetId, linkSelector) {
+    const targetElement = document.getElementById(targetId);
+    const navLink = document.querySelector(linkSelector);
+
+    if (targetElement && navLink) {
+      // The IntersectionObserver is efficient because
+      // it doesn't fire events on every pixel of scroll
+      // but only when elements cross over
+      // specific thresholds you set.
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              navLink.classList.add("highlight");
+            } else {
+              navLink.classList.remove("highlight");
+            }
+          });
+        },
+        {
+          root: null, // viewport
+
+          // You can adjust the threshold in the
+          // observer options to change when the
+          // highlighting occurs
+          // e.g., when 10% or 75% of the element is visible
+          threshold: 0.5, // trigger when 50% of the target is visible
+        }
+      );
+
+      observer.observe(targetElement);
+    }
+  }
+
+  // Highlighting for projects
+  // When a new project is added, this needs to be updated.
+  linkHighlighting(
+    "service-task-calculator",
+    'a[href="#service-task-calculator"]'
+  );
+  linkHighlighting("document-generator", 'a[href="#document-generator"]');
+  linkHighlighting("bid-generator", 'a[href="#bid-generator"]');
+  linkHighlighting("risk-category-matrix", 'a[href="#risk-category-matrix"]');
+});
